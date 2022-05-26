12e24f26a4de45f7b99935b41c42d3e042f42fb5
KimWonHo11 <dnjsgh1707@naver.com>
2022-05-26 10:36:36 +0900

    added0526

---
+++ src/gui/MemberAdder.java
@@ -0,0 +1,69 @@
+package gui;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
+
+public class MemberAdder extends JFrame {
+	
+	public MemberAdder() {
+		JPanel panel = new JPanel();
+		panel.setLayout(new SpringLayout());
+		
+		JLabel labelID = new JLabel("ID: " , JLabel.TRAILING);
+		JTextField fieldID = new JTextField(10);
+		labelID.setLabelFor(fieldID);
+		panel.add(labelID);
+		panel.add(fieldID);
+		
+		JLabel labelName = new JLabel("Name: " , JLabel.TRAILING);
+		JTextField fieldName = new JTextField(10);
+		labelName.setLabelFor(fieldName);
+		panel.add(labelName);
+		panel.add(fieldName);
+		
+		JLabel labelHeight = new JLabel("Height: " , JLabel.TRAILING);
+		JTextField fieldHeight = new JTextField(10);
+		labelHeight.setLabelFor(fieldHeight);
+		panel.add(labelHeight);
+		panel.add(fieldHeight);
+		
+		JLabel labelWeight = new JLabel("Weight: " , JLabel.TRAILING);
+		JTextField fieldWeight = new JTextField(10);
+		labelWeight.setLabelFor(fieldWeight);
+		panel.add(labelWeight);
+		panel.add(fieldWeight);
+		
+		JLabel labelBmi = new JLabel("Bmi: " , JLabel.TRAILING);
+		JTextField fieldBmi = new JTextField(10);
+		labelBmi.setLabelFor(fieldBmi);
+		panel.add(labelBmi);
+		panel.add(fieldBmi);
+		
+		JLabel labelPhoneNumber = new JLabel("PhoneNumber: " , JLabel.TRAILING);
+		JTextField fieldPhoneNumber = new JTextField(10);
+		labelPhoneNumber.setLabelFor(fieldPhoneNumber);
+		panel.add(labelPhoneNumber);
+		panel.add(fieldPhoneNumber);
+		
+		JLabel labelProgramName = new JLabel("ProgramName: " , JLabel.TRAILING);
+		JTextField fieldProgramName = new JTextField(10);
+		labelProgramName.setLabelFor(fieldProgramName);
+		panel.add(labelProgramName);
+		panel.add(fieldProgramName);
+		
+		panel.add(new JButton("save"));
+		panel.add(new JButton("cancel"));
+		
+		SpringUtillities.makeCompactGrid(panel, 8, 2, 6, 6, 6, 6);
+		
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(panel);
+		this.setVisible(true);
+		
+	}
+}
---
+++ src/gui/MemberViewer.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+public class MemberViewer extends JFrame {
+
+	MemberViewer(){
+		DefaultTableModel model = new DefaultTableModel();
+		model.addColumn("Id");	
+		model.addColumn("Name");
+		model.addColumn("Height");
+		model.addColumn("Weight");
+		model.addColumn("Bmi");
+		model.addColumn("PhoneNumber");
+		model.addColumn("ProgramName");
+		model.addColumn("CONTACT Info");
+		
+		JTable table = new JTable(model);
+		JScrollPane sp = new JScrollPane(table);
+		
+		this.add(sp);
+		this.setSize(300, 300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+}
